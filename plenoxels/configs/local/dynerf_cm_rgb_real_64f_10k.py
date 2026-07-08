b58469a9e706c4f62a9ba6560ce1b2aa7b9fbe6e
CrazyDave53 <minhchau3c@gmail.com>
2026-07-09 00:53:39 +0700

    chore: add known-good coffee martini rgb 64-frame config

---
+++ plenoxels/configs/local/dynerf_cm_rgb_real_64f_10k.py
@@ -0,0 +1,37 @@
+from plenoxels.configs.final.DyNeRF.dynerf_hybrid import config
+
+config = dict(config)
+
+config["expname"] = "cm_rgb_real_64f_10k"
+config["logdir"] = "./logs/baseline"
+config["device"] = "cuda:0"
+
+config["data_dirs"] = ["data/neu3d/coffee_martini"]
+config["data_downsample"] = 2
+config["contract"] = False
+config["ndc"] = True
+config["ndc_far"] = 2.6
+config["near_scaling"] = 0.9
+config["scene_bbox"] = [[-3.0, -1.8, -1.2], [3.0, 1.8, 1.2]]
+
+config["isg"] = False
+config["isg_step"] = -1
+config["ist_step"] = -1
+config["keyframes"] = False
+
+config["max_train_cameras"] = None
+config["max_train_tsteps"] = 64
+config["max_test_cameras"] = 1
+config["max_test_tsteps"] = 8
+
+config["num_steps"] = 10000
+config["batch_size"] = 4096
+config["save_every"] = 10000
+config["valid_every"] = -1
+config["save_outputs"] = False
+config["train_fp16"] = True
+
+config["optim_type"] = "adam"
+config["scheduler_type"] = "warmup_cosine"
+config["lr"] = 0.01
+
